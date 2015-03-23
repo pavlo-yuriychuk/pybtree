870691a4ab1e77c4e3586ebdd62fb64d5e8619c0
Pavlo Iuriichuk <pluccky@gmail.com>
2015-03-23 17:22:56 +0200

    Fixed iteration over children

--- btree/__init__.py
+++ btree/__init__.py
@@ -1,50 +1,52 @@
 
+from itertools import imap, chain, starmap
+
+def isingle(item):
+    yield item
+
+def ichild(item):
+    if item is None:
+        return [item]
+    else:
+        return iter(item)
+
 class TreeNode:
 
-	def __init__(self, value, left = None, right = None):
-		self.value = value
-		self.left = left
-		self.right = right
+    def __init__(self, value, left = None, right = None, level = 0):
+        self.value = value
+        self.parent = None
+        self.level = level
+        self.children = []
+        self.add_child(left)
+        self.add_child(right)
 
-	def is_leaf(self):
-		return self.left is None and self.right is None
+    def add_child(self, node):
+        if node is not None:
+            node.set_parent(self)
+        self.children.append(node)
 
-	def __iter__(self):
-		if self.is_leaf():
-			yield self.value
-		else:
-			children = []
-			if self.left is not None:
-				for child in self.left:
-					children.append(child)
-			yield self.value
-			if self.right is not None:
-				for child in self.right:
-					children.append(child)
-			yield children
+    def is_leaf(self):
+        return len(self.children) == 0
 
-	def to_list(self):
-		result = []
-		level = 0
+    def is_root(self):
+        return self.parent is None
 
-		def flatten(value, level):
-			if isinstance(value, list):
-				for item in value:
-					flatten(item, level + 1)
-			else:
-				if level >= len(result):
-					for i in range(len(result), level):
-						result.append([])
-				result[level - 1].append(value)
+    def set_parent(self, value):
+        self.parent = value
+        self.level = self.parent.level + 1
 
-		items = [child for child in self]
-		flatten(items, 0)
-		return result
+    def __iter__(self):
+        return chain(*([isingle((self.value, self.level))] + map(ichild, self.children)))
+
+    def __repr__(self):
+        return str(map(str, self))
+
+
 
 if __name__ == "__main__":
-	assert TreeNode(0, TreeNode(1, None, TreeNode(2)), TreeNode(3, TreeNode(4), TreeNode(5, TreeNode(6)))).to_list() == [[0], [1, 3], [2, 4, 5], [6]]
-	assert TreeNode(0, TreeNode(1, TreeNode(5), TreeNode(6, TreeNode(7))), TreeNode(2, TreeNode(3), TreeNode(4))).to_list() == [[0], [1, 2], [5, 6, 3, 4], [7]]
-	assert TreeNode(0, TreeNode(1), TreeNode(2)).to_list() == [[0], [1, 2]]
-	assert TreeNode(0).to_list() == [[0]]
-	assert TreeNode(0).is_leaf() == True
-	assert TreeNode(0, None, TreeNode(1)).is_leaf() == False
+    assert TreeNode(0, TreeNode(1, None, TreeNode(2)), TreeNode(3, TreeNode(4), TreeNode(5, TreeNode(6)))).to_list() == [[0], [1, 3], [2, 4, 5], [6]]
+    assert TreeNode(0, TreeNode(1, TreeNode(5), TreeNode(6, TreeNode(7))), TreeNode(2, TreeNode(3), TreeNode(4))).to_list() == [[0], [1, 2], [5, 6, 3, 4], [7]]
+    assert TreeNode(0, TreeNode(1), TreeNode(2)).to_list() == [[0], [1, 2]]
+    assert TreeNode(0).to_list() == [[0]]
+    assert TreeNode(0).is_leaf() == True
+    assert TreeNode(0, None, TreeNode(1)).is_leaf() == False
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+#!/usr/env python
 from btree import TreeNode
 
 def main():
-	print TreeNode(0, TreeNode(1, None, TreeNode(2)), TreeNode(3, TreeNode(4), TreeNode(5, TreeNode(6)))).to_list()
-	print TreeNode(0, TreeNode(1, TreeNode(5), TreeNode(6, TreeNode(7))), TreeNode(2, TreeNode(3), TreeNode(4))).to_list()
+    for i in TreeNode(0, TreeNode(1, None, TreeNode(2)), TreeNode(3, TreeNode(4), TreeNode(5, TreeNode(6)))):
+        print i
+    print TreeNode(0, TreeNode(1, TreeNode(5), TreeNode(6, TreeNode(7))), TreeNode(2, TreeNode(3), TreeNode(4)))
 
 if __name__ == "__main__":
-	main()
+    main()
